6866d2bd945b885e7e91c8ce57fb88833e549e8d
michintosh <michelangelo.alesi.95@gmail.com>
2020-05-22 23:51:58 +0200

    Modified single.html to receive title and content from md into a div, turned the div into a draggable

--- static/js/windows-gen.js
+++ static/js/windows-gen.js
@@ -1,6 +1,9 @@
+/*
 var newPJWindow = document.createElement('div');
 var closeButton = document.createElement("button");
 var pjWinOpened = false;
+
+//console.log(document.querySelector(".draggable-window"))
 
 function GenerateProjectWindow()
 {
@@ -19,51 +22,50 @@ function GenerateProjectWindow()
 
 pjWinOpened = true;
 }
-   // Make the DIV element draggable:
-   dragElement(document.getElementById("project"));
 
-   function dragElement(elmnt) {
-     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-       elmnt.onmousedown = dragMouseDown;
-
-
-     function dragMouseDown(e) {
-       e = e || window.event;
-       e.preventDefault();
-       // get the mouse cursor position at startup:
-       pos3 = e.clientX;
-       pos4 = e.clientY;
-       document.onmouseup = closeDragElement;
-       // call a function whenever the cursor moves:
-       document.onmousemove = elementDrag;
-     }
-
-     function elementDrag(e) {
-       e = e || window.event;
-       e.preventDefault();
-       // calculate the new cursor position:
-       pos1 = pos3 - e.clientX;
-       pos2 = pos4 - e.clientY;
-       pos3 = e.clientX;
-       pos4 = e.clientY;
-       // set the element's new position:
-       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-     }
-
-     function closeDragElement() {
-       // stop moving when mouse button is released:
-       document.onmouseup = null;
-       document.onmousemove = null;
-     }
-   }
+document.querySelector(".draggable-window").onmousedown = 
+nsole.log(elmnt)
 }
+*/
+dragElement(document.querySelector("#draggable-window"))
 
-function closeWindow()
-{
-  document.getElementById("project").innerHTML=" ";
-  document.getElementById("project").remove();
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("window-header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("window-header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-  pjWinOpened = false;
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
